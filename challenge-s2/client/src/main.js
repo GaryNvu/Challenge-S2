a53272fc9b3e42bf175dc7bd322ecefa9754d495
nghuyhoang <Aries.THs@gmail.com>
2024-06-03 22:30:06 +0200

    Add .gitignore file

--- challenge-s2/client/src/main.js
+++ challenge-s2/client/src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import * as VueRouter from 'vue-router';
+import ShoppingCartPage from './page/ShoppingCartPage.vue';
+import ProductsPage from './page/ProductsPage.vue';
+import ProductDetailPage from './page/ProductDetailPage.vue';
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(VueRouter.createRouter({
+        history: VueRouter.createWebHistory(),
+        routes: [{
+            path: '/cart',
+            component: ShoppingCartPage,
+        },
+        {
+            path: '/products',
+            component: ProductsPage,
+        },
+        {
+            path: '/product/:id',
+            component: ProductDetailPage,
+        },
+        ]
+    }))
+    .mount('#app')
