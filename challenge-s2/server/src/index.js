3fff19df6c93f2dce2e853de73b0c409a68fb3b1
nghuyhoang <Aries.THs@gmail.com>
2024-06-11 00:16:35 +0200

    Premier pas du backend avec des endpoints

--- challenge-s2/server/src/index.js
+++ challenge-s2/server/src/index.js
@@ -3,7 +3,21 @@ import { indexRouter } from "./routes/index.js";
 
 const server = express();
 
-server.use("/", indexRouter);
+server.use("/", (req, res) => {
+  res.send("Hello!");
+});
+
+server.get('/products', (req, res) => {
+
+})
+
+server.get('/cart', (req, res) => {
+
+})
+
+server.get('/products/:productid', (req, res) => {
+
+})
 
 server.listen(8000, "0.0.0.0", () => {
   console.log("Server listening on http://localhost:8000");
