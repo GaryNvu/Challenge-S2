745b82d077656b0af86910e28d83818deaa1d48c
nghuyhoang <Aries.THs@gmail.com>
2024-06-12 01:35:02 +0200

    Developer endpoints et refactoring code

--- challenge-s2/server/src/index.js
+++ challenge-s2/server/src/index.js
@@ -1,24 +1,58 @@
 import express from "express";
-import { indexRouter } from "./routes/index.js";
+import { cartItems as cartItemsRaw, products as productsRaw } from "./temp-data.js";
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
 
 const server = express();
+server.use(express.json()); // Middleware to parse JSON bodies
 
-server.use("/", (req, res) => {
+// Root route
+server.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Route to get all products
 server.get('/products', (req, res) => {
+  res.json(products);
+});
 
-})
-
+function populateCartIds(ids) {
+  return ids.map(id => products.find(product => product.id === id))
+}
+// Route to get all cart items
 server.get('/cart', (req, res) => {
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
 
-})
-
+// Route to get a specific product by ID
 server.get('/products/:productid', (req, res) => {
+  const productid = req.params.productid;
+  const product = products.find(product => product.id === productid);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Product not found');
+  }
+});
 
+// Route to add a product to the cart
+server.post('/cart', (req, res) => {
+  const productid = req.body.id;
+  cartItems.push(productid);
+  const populateCart = populateCartIds(cartItems);
+  res.json(populateCart);
+});
+
+server.delete('/cart/:productid', (req, res) => {
+  const productid = req.params.productid;
+  cartItems = cartItems.filter(id => id !== productid)
+  const populateCart = populateCartIds(cartItems);
+  res.json(populateCart);
 })
 
+// Start the server
 server.listen(8000, "0.0.0.0", () => {
   console.log("Server listening on http://localhost:8000");
 });
---
+++ challenge-s2/server/src/temp-data.js
@@ -0,0 +1,43 @@
+export const cartItems = ['123', '234', '345'];
+
+export const products = [{
+    id: '123',
+    name: 'Boite-Cart1',
+    price: '$26.00',
+    amount: 0,
+}, {
+    id: '234',
+    name: 'Boite-Cart2',
+    price: '$26.00',
+    amount: 0,
+}, {
+    id: '345',
+    name: 'Boite-Cart3',
+    price: '$26.00',
+    amount: 0,
+}, {
+    id: '456',
+    name: 'Boite-Cart4',
+    price: '$26.00',
+    amount: 0,
+}, {
+    id: '567',
+    name: 'Boite-Cart5',
+    price: '$26.00',
+    amount: 0,
+}, {
+    id: '678',
+    name: 'Boite-Cart6',
+    price: '$24.50',
+    amount: 0,
+}, {
+    id: '789',
+    name: 'Boite-Cart7',
+    price: '$24.50',
+    amount: 0,
+}, {
+    id: '890',
+    name: 'Boite-Cart8',
+    price: '$24.50',
+    amount: 0,
+}];
