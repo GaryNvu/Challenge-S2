b2a928deee8c9cc7ef192af4ea7bb9be93f1eb1a
Gary Neveu <neveugary14@gmail.com>
2024-06-11 21:55:12 +0200

    navbar v1

--- challenge-s2/client/src/App.vue
+++ challenge-s2/client/src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <Header />
-  <div>
-    <div class="page-wrap">
+  <div class="app">
+    <Header @toggle-menu="toggleMenu" />
+    <div class="content">
+      <Sidebar :is_expanded="is_expanded" />
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue'
-  export default {
-    name: "App",
-    components: {
-      Header,
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Sidebar,
+  },
+  data() {
+    return {
+      is_expanded: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.is_expanded = !this.is_expanded;
     },
+  },
 };
 </script>
+
+<style scoped>
+.content {
+  display: flex;
+}
+</style>
--- challenge-s2/client/src/main.js
+++ challenge-s2/client/src/main.js
@@ -2,37 +2,10 @@ import { createApp } from 'vue';
 import './style.css';
 import './main.css';
 import App from './App.vue';
+import router from './router'
 
-import * as VueRouter from 'vue-router';
-import ShoppingCartPage from './page/ShoppingCartPage.vue';
-import ProductsPage from './page/ProductsPage.vue';
-import ProductDetailPage from './page/ProductDetailPage.vue';
-import PageNotFound from './page/PageNotFound.vue';
-
-const routes = [
-    {
-        path: '/cart',
-        component: ShoppingCartPage,
-    },
-    {
-        path: '/products',
-        component: ProductsPage,
-    },
-    {
-        path: '/products/:productid',
-        component: ProductDetailPage,
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        component: PageNotFound,
-    }
-];
-
-// Création du routeur
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
-    routes,
-});
+import "bootstrap/dist/css/bootstrap.css";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 
 // Création et montage de l'application Vue
 const app = createApp(App);
