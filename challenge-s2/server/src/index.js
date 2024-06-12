ce9dd6ea78d7a985d86921c21bfaeb7d51d544ff
nghuyhoang <Aries.THs@gmail.com>
2024-06-13 01:26:11 +0200

    Connect mongodb vers express

--- challenge-s2/server/src/index.js
+++ challenge-s2/server/src/index.js
@@ -1,15 +1,20 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 import { cartItems as cartItemsRaw, products as productsRaw } from "./temp-data.js";
 
 let cartItems = cartItemsRaw;
 let products = productsRaw;
-
+const url = `mongodb+srv://challenge-s2:M8P1acAtfshWAE7w@cluster0.uzoqctd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const client = new MongoClient(url)
 const server = express();
 server.use(express.json()); // Middleware to parse JSON bodies
 
 // Root route
-server.get("/", (req, res) => {
-  res.send("Hello!");
+server.get("/hello", async (req, res) => {
+  await client.connect();
+  const db = client.db('fsv-db');
+  const products = await db.collection('products').find({}).toArray();
+  res.send(products);
 });
 
 // Route to get all products
--- challenge-s2/server/src/temp-data.js
+++ challenge-s2/server/src/temp-data.js
@@ -4,40 +4,40 @@ export const products = [{
     id: '123',
     name: 'Boite-Cart1',
     price: '$26.00',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart1.png'
 }, {
     id: '234',
     name: 'Boite-Cart2',
     price: '$26.00',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart2.png'
 }, {
     id: '345',
     name: 'Boite-Cart3',
     price: '$26.00',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart3.png'
 }, {
     id: '456',
     name: 'Boite-Cart4',
     price: '$26.00',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart4.png'
 }, {
     id: '567',
     name: 'Boite-Cart5',
     price: '$26.00',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart5.png'
 }, {
     id: '678',
     name: 'Boite-Cart6',
     price: '$24.50',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart6.png'
 }, {
     id: '789',
     name: 'Boite-Cart7',
     price: '$24.50',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart7.png'
 }, {
     id: '890',
     name: 'Boite-Cart8',
     price: '$24.50',
-    amount: 0,
+    imageUrl: '/images/Boite-Cart8.png'
 }];
