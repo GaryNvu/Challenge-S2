fadfb994302385a7cee4e74bfcb68c6c3602d980
Gary Neveu <garyneveu14@gmail.com>
2024-06-18 10:35:01 +0200

    show stock, quantity link to stock

--- challenge-s2/client/src/components/ProductsList.vue
+++ challenge-s2/client/src/components/ProductsList.vue
@@ -30,10 +30,13 @@
                 <div class="card-body">
                     <h5 class="card-title">{{ product.name }}</h5>
                     <p class="card-text">Price : {{ product.price }} €</p>
+                    <p :class="{'text-success': product.stock > 0, 'text-danger': product.stock <= 0}">
+                        {{ product.stock > 0 ? "En stock" : "Epuisé" }}
+                    </p>
                 </div>
                 <div class="d-flex justify-content-between align-items-center m-3">
                     <div>Quantity</div>
-                    <el-input-number v-model="product.amount" :min="0" controls-position="right" size="default" />
+                    <el-input-number v-model="product.amount" :min="0" :max="product.stock" controls-position="right" size="default" />
                     <button class="btn btn-primary btn-sm" @click="addToCart(product.id, product.amount)">
                         Add
                     </button>
@@ -91,5 +94,11 @@ export default {
 .card-img-top {
     max-width: 100%;
     max-height: 100%;
+}
+.text-success {
+    color: green;
+}
+.text-danger {
+    color: red;
 }
 </style>
--- challenge-s2/client/src/page/ProductDetailPage.vue
+++ challenge-s2/client/src/page/ProductDetailPage.vue
@@ -12,12 +12,19 @@
               <div class="d-flex flex-column justify-content-center h-75">
                 <h3>Prix : {{ product.price }} €</h3>
                 <h3>Etat : {{ product.condition }}</h3>
-                <h3>Langue : {{ product.language }}</h3>  
+                <h3>Langue : {{ product.language }}</h3>
+                <div class="d-flex align-items-center">
+                  <i v-if="product.stock > 0" class="bi bi-check fs-2 text-success"></i>
+                  <i v-else class="bi bi-x fs-2 text-danger"></i>
+                  <h3 :class="{'text-success': product.stock > 0, 'text-danger': product.stock <= 0}" class="mb-0">
+                        {{ product.stock > 0 ? "En stock" : "Epuisé" }}
+                  </h3>
+                </div>
               </div>
               <div class="d-flex flex-column">
                 <div class="quantity d-flex align-items-center">
                   <p class="mt-3 align-self-center">Quantity : </p>
-                  <el-input-number class="" v-model="product.amount" controls-position="right" :min="0" size="medium" />
+                  <el-input-number class="" v-model="product.amount" controls-position="right" :min="0" :max="product.stock" size="default" />
                 </div>
                 <button class="btn btn-primary mt-auto mb-0 pl-4" @click="addToCart(product.id, product.amount)">Add to cart</button>
               </div>
