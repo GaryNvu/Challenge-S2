d9e42e4ea776a43cf198a63ddb2eb8ca966c78c7
Gary Neveu <garyneveu14@gmail.com>
2024-07-09 11:26:03 +0200

    restart server to SQL only, route users and products

--- challenge-s2/server/src/routes/products.js
+++ challenge-s2/server/src/routes/products.js
@@ -3,10 +3,10 @@ import pool from '../db.js'; // Assurez-vous d'utiliser l'extension de fichier
 
 const router = Router();
 
-// GET user
-router.get('/users', async (req, res) => {
+// GET ALL products
+router.get('/products', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
+    const result = await pool.query('SELECT * FROM products');
     res.json(result.rows);
   } catch (err) {
     console.error(err.message);
@@ -14,13 +14,13 @@ router.get('/users', async (req, res) => {
   }
 });
 
-// POST user
-router.post('/users', async (req, res) => {
-    const { username, email, password } = req.body;
+// POST product
+router.post('/products', async (req, res) => {
+    const { name, price, brand, categorie, description, weight, stock } = req.body;
     try {
       const result = await pool.query(
-        'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
-        [username, email, password]
+        'INSERT INTO products (name, price, brand, categorie, description, weight, stock) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+        [name, price, brand, categorie, description, weight, stock]
       );
       res.json(result.rows[0]);
     } catch (err) {
