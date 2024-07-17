b09280661477bcc25161c829a2d6ed325b2cc604
Gary Neveu <garyneveu14@gmail.com>
2024-07-17 21:16:02 +0200

    redesign page

--- challenge-s2/client/src/page/PageNotFound.vue
+++ challenge-s2/client/src/page/PageNotFound.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>Error 404 - Page Not Found </h1>
+  <div class="d-flex justify-content-center flex-column align-items-center">
+    <img class="mt-5" src="../assets/little-creature-removebg.png" alt="Little creature with cards around him">
+    <h1 class="text-primary">404</h1>
+    <h2 class="text-primary">Page Not Found</h2>
+    <router-link to="/" title="Accueil" class="">
+      <button class="btn btn-primary fs-3 mt-4">
+        Accueil
+      </button>
+    </router-link>
   </div>
 </template>
 
@@ -12,7 +19,15 @@ export default {
 
 <style scoped>
 
-h1 {
-  font-size: 3em;
-}
+  h1 {
+    font-size: 4.5em;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  img {
+    width: 20%;
+  }
 </style>
