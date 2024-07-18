f43a89a2aad17781b146a346fa60c118e10f4347
nghuyhoang <Aries.THs@gmail.com>
2024-07-18 23:24:00 +0200

    Update test for product.test.js

--- challenge-s2/server/src/service/denormalization/product.js
+++ challenge-s2/server/src/service/denormalization/product.js
@@ -1,37 +1,34 @@
-const { Types: { ObjectId } } = require('mongoose');
-const MongoProduct = require("../../mongo/Product");
+const mongoose = require('mongoose');
+const MongoProduct = require('../../mongo/Product');
 
 const denormalizeProduct = async (productId, Product) => {
   try {
-    const product = await Product.findByPk(productId, {
-      include: ['Category', 'Brand']
-    });
-
-    if (product) {
-      console.log(`Denormalizing product with ID: ${productId}`);
-
-      const mongoProductId = new ObjectId(productId);
-
-      await MongoProduct.findOneAndUpdate(
-        { _id: mongoProductId },
-        {
-          sqlID: productId,
-          name: product.name,
-          price: product.price,
-          brand: product.Brand.name,
-          category: product.Category.name,
-          description: product.description,
-          weight: product.weight,
-          stock: product.stock,
-          image: product.image,
-        },
-        { upsert: true }
-      );
-      console.log(`Product with ID: ${productId} denormalized successfully`);
+    const product = await Product.findByPk(productId, { include: ["Category", "Brand"] });
+    if (!product) {
+      console.log(`Product with ID: ${productId} not found`);
+      return;
     }
+
+    const { name, price, brand, category, description, weight, stock, image } = product;
+
+    await MongoProduct.findOneAndUpdate(
+      { _id: new mongoose.Types.ObjectId(productId) },
+      {
+        name,
+        price,
+        brand,
+        category,
+        description,
+        weight,
+        stock,
+        image,
+      },
+      { upsert: true }
+    );
+    console.log(`Successfully denormalized product with ID: ${productId}`);
   } catch (err) {
     console.error(`Error denormalizing product: ${err.message}`);
   }
 };
-  
+
 module.exports = denormalizeProduct;
