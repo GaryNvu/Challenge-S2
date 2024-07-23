773dff1b36b4b8c8f1b499d4dba03826168d9631
Gary Neveu <garyneveu14@gmail.com>
2024-07-23 14:57:08 +0200

    fix

--- server/build/mongo/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var mongoConnection = require('./db');
-var Product = require('./Product')(mongoConnection);
-var db = {
-  Product: Product,
-  mongoConnection: mongoConnection
-};
-module.exports = db;
