13d1c9de818ec6fc597bf53460302c6da0d0cc97
Swartline <77667035+Swartline@users.noreply.github.com>
2024-07-24 21:57:54 +0200

    Dashboard without charts

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import router from './router';
+import router from './router'
+import mitt from 'mitt'
 import store from './data/store';
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -11,8 +12,11 @@ import '@fortawesome/fontawesome-free/js/all'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+const emitter = mitt()
+
 // Création et montage de l'application Vue
 const app = createApp(App);
+app.config.globalProperties.emitter = emitter
 app.use(router);
 app.use(store);
 app.use(ElementPlus);
